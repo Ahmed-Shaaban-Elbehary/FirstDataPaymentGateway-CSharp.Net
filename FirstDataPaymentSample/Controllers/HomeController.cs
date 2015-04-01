29b42203efef23e941df068459292aac00e166a2
deepumi <deepumi@gmail.com>
2015-04-01 14:25:49 -0400

    Initial Commit

---
+++ FirstDataPaymentSample/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using FirstDataPaymentSample.Api;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace FirstDataPaymentSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var amount = "1.66";
+            var cardExpirtyDate = "0515";
+            var cardHolderName = "Card Holder Name";
+            var cardNumber = "4111111111111111";
+            var customerId = 23;//Internal purpose to track which customer made the transaction....
+            var paymentService = new PaymentService(amount, cardExpirtyDate, cardHolderName, cardNumber, customerId.ToString());
+            var transactionResult = await paymentService.PostAsync();
+            if (transactionResult == null) return View();
+            if (transactionResult.TransactionApproved && !transactionResult.TransactionError 
+                    && transactionResult.CustomerRef == customerId.ToString())
+            {
+                //transaction success....
+                TempData["TRANSACTION_STATUS"] = "APPROVED";
+            }
+            else
+            {
+                var message = transactionResult.Message;
+            }
+            return View();
+        }
+    }
+}
